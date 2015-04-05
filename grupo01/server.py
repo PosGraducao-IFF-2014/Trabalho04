51c053f760e9205cb3551b5bdae9dcea2570a815
Fiuza <frederico.fiuza@hesea.com.br>
2015-04-04 21:58:01 -0300

    Lista Estoque

--- grupo01/server.py
+++ grupo01/server.py
@@ -4,6 +4,7 @@ import urllib
 from urllib2 import URLError
 from urllib2 import HTTPError
 import urllib2
+import json
 
 
 @post('/cadastraEstoque')
@@ -11,14 +12,14 @@ def cadastraEstoque(db):
 	codigo = request.forms.get('codigo')
 	localizacao = request.forms.get('localizcao')
 	descricao 	= request.forms.get('descricao')
-	#c = db.execute('INSERT INTO Estoque (codigo, descricao, localizacao) VALUES (codigo,\'descricao\',\'localizacao\')')
-	db.execute("INSERT INTO Estoque (codigo, descricao, localizacao) values ('%s' ,'%s', '%s')"%(codigo,localizacao, descricao))
+	c = db.execute('INSERT INTO Estoque (codigo, descricao, localizacao) VALUES (codigo,\'descricao\',\'localizacao\')')
+		
 	print('Cadastrado com suscess')
 
-@route('/delete/<post_id:int>'
+@route('/delete/<post_id:int>')
 def deleteEstoque(db,id):
 		try:
-	        url = "http://localhost:8004/teste/"+str(post_id)
+	        url = "http://localhost:8004/consulta_estoque/"+str(post_id)
 	        request = urllib2.Request(url)
 
 	        # Abre a conexão
@@ -40,5 +41,14 @@ def deleteEstoque(db,id):
 	    except URLError, e:
 	        print("URL inválido!\n")
 	        print("Mensagem: ", e.reason)
+
+@get('/listaEstoque')
+def listaEstoque(db):
+	response.content_type = 'application/json'
+	c = db.execute('SELECT * FROM Estoque ORDER BY descricao ASC ')
+	retornoEstoque = []
+	for row in db.execute('SELECT * FROM stocks ORDER BY price'):
+		retornoEstoque.append(row)
+    return json.dump(retornoEstoque)
 
 run(reloader=True, host='localhost', port=8001,debug=True)
