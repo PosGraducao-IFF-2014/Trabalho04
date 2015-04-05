21d36f3fb9c90f14657c6233179be11e3a92370c
faelvs <faelvs@yahoo.com.br>
2015-04-04 21:34:03 -0300

    Update server.py

--- grupo07/server.py
+++ grupo07/server.py
@@ -1,21 +1,25 @@
 from bottle import route, install, template, run, get, post, response, request
 from bottle_sqlite import SQLitePlugin
 from flask_table import Table, Col
-
-
-
 install(SQLitePlugin(dbfile='compras.sqlite'))
-
-
 @route('/existe/<post_id:int>')
 def existe(db, post_id):
-    response.content_type = 'application/json'
-    c = db.execute('SELECT codigoCompra, codigoFornecedor FROM compras WHERE codigoFornecedor = ?', (post_id,))
-    row = c.fetchone()
-    if row==None:
-        return False
-    else:
-        return True
-
-
+response.content_type = 'application/json'
+c = db.execute('SELECT codigoCompra, codigoFornecedor FROM compras WHERE codigoFornecedor = ?', (post_id,))
+row = c.fetchone()
+if row==None:
+return False
+else:
+return True
 run(reloader=True, host='localhost', port=8007,debug=True)
+
+def compras(db):
+codigoCompra = request.forms.get('codigoCompra')
+codigoProduto = request.forms.get('codigoProduto')
+codigoFornecedor = request.forms.get('codigoFornecedor')
+data = request.forms.get('data')
+quantidade = request.forms.get('quantidade')
+valortotal = request.forms.get('valortotal')
+#c = db.execute('INSERT INTO compras (codigoCompra, codigoProduto, codigoFornecedor, data, quantidade, valortotal) VALUES (codigoCompra,codigoProduto,codigoFornecedor,\'data'\,quantidade,valortotal)')
+db.execute("INSERT INTO compras (codigoCompra, codigoProduto, codigoFornecedor, data, quantidade, valortotal) values ('%s' ,'%s', '%s', '%s', '%s', '%s')"%(codigoCompra, codigoProduto, codigoFornecedor, data, quantidade, valortotal))
+print('Compra cadastradacom sucesso!')
