09d85ac73d853c7728ef7684befd1934a08e8bd8
tgabry <thaiscgabry@live.com>
2015-04-06 10:26:03 -0300

    Verificando codigo

--- grupo09/server.py
+++ grupo09/server.py
@@ -1,11 +1,26 @@
 from bottle import route, install, template, run, get, post, response, request
 from bottle_sqlite import SQLitePlugin
+import urllib
+import json
 
-install(SQLitePlugin(dbfile='db.sqlite'))
+@post('/cadastra_venda')
+def venda(db):
+  codigo_venda = request.forms.get('codigo_venda')
+  codigo_cliente = request.forms.get('codigo_cliente')
+  codigo_funcionario = request.forms.get('codigo_funcionario')
+  data = request.forms.get('data')
+  valor_total = request.forms.get('valor_total')
+  codigo_produto = request.forms.get('codigo_produto')
+  quantidade = request.forms.get('quantidade')
+  db.execute("INSERT INTO venda(codigo_venda,codigo_cliente,codigo_funcionario,data,valor_total,codigo_produto,quantidade) values ('%s' ,'%s', '%s', '%s', '%s', '%s', '%s')"%(codigo_venda,codigo_cliente,codigo_funcionario,data,valor_total,codigo_produto,quantidade)) 
+  print('Cadastrado!')
 
-@get('/consulta_venda/<codigo_venda>')
-def consulta_venda(db, codigo_venda):
-    total = db.execute("""
-    select count(*) as total from venda
-    where codigo_venda = %s""" % (codigo_venda,)).fetchone()['total']
-    return '1' if total > 0 else '0'
+@get('/consulta/<post_id:int>')
+def venda(db,post_id):
+  response.content_type = 'application/json'
+  v = db.execute('SELECT venda, codigo_venda FROM venda WHERE id = ?', (post_id))
+  row = v.fetchone()
+  return {'codigo_venda'}
+
+
+run(reloader=True, host='localhost', port=8009, debug=True)
