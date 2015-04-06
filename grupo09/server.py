45649ffc9cd51fa415a208c8bd4efdaeab7ddc69
tgabry <thaiscgabry@live.com>
2015-04-06 15:23:59 -0300

    Tags

--- grupo09/server.py
+++ grupo09/server.py
@@ -3,7 +3,6 @@ from bottle_sqlite import SQLitePlugin
 from flask_table import Table, Col
 install(SQLitePlugin(dbfile='venda.sqlite'))
 
-
 @post('/cadastra')
 def venda_cadastro(db):
   codigo_venda = request.forms.get('codigo_venda')
@@ -19,33 +18,6 @@ def venda_cadastro(db):
   except Exception, e:
     return e
 
-@get('/consultaVenda')
-def consulta_form():
-    return '''  <h1>Consulta de Produto</h1></p>
-                <form method="POST" action="/consultar">
-                Codigo : <input name="codigo"     type="text" /></p>
-                <input type="submit" />
-              </form>
-                '''
 
-@post('/consultar')
-def consulta_submit(db):
-  consulta_html ='''  <h1>Produto Consultado</h1></p>
-                <form >
-                    Codigo : {{codigo}}</p>
-                    Descricao : {{descricao}}</p>
-                    Preco : {{preco}}</p>
-                    Codigo Fabricante : {{codigoFabricante}}</p>
-                    <a href="/consultaProduto">Voltar</a></p>
-                    <a href="/cadastraProduto">Cadastrar Novo Produto</a>
-                </form>
-                '''
-  codigo     = request.forms.get('codigo_venda')
-  try:
-    c = db.execute('SELECT codigo_venda FROM venda WHERE codigo_venda = ?', (codigo_venda,))
-    row = c.fetchone()
-    return template(consulta_html, codigo_venda=row['codigo'], codigo_funcionario=row['descricao'])
-  except Exception, e:
-    return '''<p>Produto nao encontrado</p>'''
 
 run(reloader=True, host='localhost', port=8009, debug=True)
