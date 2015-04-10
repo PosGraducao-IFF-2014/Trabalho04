d9e39c785b0945ebdb0f9231fe14881a60673ea2
ludiaz <luceliodias@gmail.com>
2015-04-10 14:23:34 -0300

    atualizando

--- grupo02/server.py
+++ grupo02/server.py
@@ -1,26 +1,85 @@
 from bottle import route, install, template, run, get, post, response, request
 from bottle_sqlite import SQLitePlugin
+from flask_table import Table, Col
 import urllib
 from urllib2 import URLError
 from urllib2 import HTTPError
 import urllib2
-import json
 
 install(SQLitePlugin(db='fabricante.db'))
 
-@post('/cadastraFabricante')
+@get('/cadastraFabricante')
+def cadastro_form():
+    return '''  <h1>Cadastro de Fabricante</h1></p>
+                <form method="POST" action="/cadastrarFabricante">
+                Codigo : <input name="codigo" type="text" /></p>
+                Descricao : <input name="descricao" type="text" /></p>
+                Localizacao : <input name="localizacao" type="text" /></p>
+                <input type="submit" />
+              </form>
+                '''
+
+@post('/cadastrarFabricante')
 def cadastraFabricante(db):
-	codigo = request.forms.get('codigo')
-	localizacao = request.forms.get('localizacao')
-	descricao 	= request.forms.get('descricao')
-	c = db.execute('INSERT INTO fabricante (codigo, descricao, localizacao) VALUES (codigo,\'descricao\',\'localizacao\')')
-		
-	print('Sucesso!')
-
-@route('/DeleteFabricante /<id:int>')
-def DeleteFabricante (db, id):
-
+    codigo = request.forms.get('codigo')
+    descricao = request.forms.get('descricao')
+    localizacao = request.forms.get('localizacao')
     try:
+        db.execute("INSERT INTO fabricante (codigo, descricao, localizacao) values ('%s', '%s', '%s')" %(codigo, descricao, localizacao))
+        return '''<p>Fabricante Cadastrado com Sucesso</p>
+                    <a href="/consultaFabricante">Consultar Fabricante</a></p>
+                    <a href="/cadastraFabricante">Cadastrar Fabricante</a>
+                    '''
+    except Exception, e:
+        return '''<p>Erro ao cadastrar Fabricante</p>
+                     <a href="/cadastraFabricante">Voltar</a>
+        '''
+
+@get('/consultaFabricante')
+def consultaFabricante():
+    return '''  <h1>Consulta de Fabricante</h1></p>
+                <form method="POST" action="/consultar">
+                Codigo : <input name="codigo" type="text" /></p>
+                <input type="submit" />
+              </form>
+                '''
+
+@post('/consultarFabricante')
+def consultarFabricante(db):
+    consulta_html ='''  <h1>Fabricante</h1></p>
+                <form >
+                    Codigo : {{codigo}}</p>
+                    Descricao : {{descricao}}</p>
+                    Localizacao : {{localizacao}}</p>
+                    <a href="/consultaFabricante">Voltar</a></p>
+                    <a href="/cadastraFabricante">Cadastrar Fabricante</a>
+                </form>
+                '''
+    codigo = request.forms.get('codigo')
+    try:
+        c = db.execute('SELECT codigo, descricao, localizacao FROM fabricante WHERE codigo = ?', (codigo))
+        row = c.fetchone()
+        return template(consulta_html, codigo=row['codigo'], descricao=row['descricao'], localizacao=row['localizacao'])
+    except Exception, e:
+        return '''<p>Fabricante nao encontrado</p>
+                      <a href="/consultaFabricante">Voltar</a></p>
+                      <a href="/cadastraFabricante">Cadastrar Fabricante</a>
+        '''
+
+@get('/deletaFabricante')
+def deletaFabricante():
+    return '''  <h1>Exclusao de Fabricante</h1></p>
+                <form method="POST" action="/deletar">
+                Codigo: <input name="codigo" type="text" /></p>
+                <input type="submit" />
+              </form>
+                '''
+
+@post('/deletarFabricante')
+def deletarFabricante(db):
+    codigo = request.forms.get('codigo')
+    try:
+
         url = "http://localhost:8003/listarProduto/"
         request = urllib2.Request(url)
 
@@ -39,25 +98,42 @@ def DeleteFabricante (db, id):
 
         if existe is False:
             db.execute('DELETE FROM fabricante where id = ?', id)
+            return '''<p>Fabricante deletado com Sucesso</p>
+                        <a href="/consultaFabricante">Consultar Fabricante</a></p>
+                        <a href="/cadastraFabricante">Cadastrar Fabricante</a></p>
+                        <a href="/deletaFabricante">Deletar Fabricante</a>
+                        '''
+        else:
+            return '''<p>N&atilde;o foi poss&iacute;vel excluir o fabricante.</p>'''
 
-        return True
-
-    except HTTPError, e:
-        print("Ocorreu um erro ao acessar o servidor!\n")
-        print("Cod.: ", e.code)
-        return False
-    except URLError, e:
-        print("URL inválida!\n")
-        print("Mensagem: ", e.reason)
-        return False
+    except Exception, e:
+        return "<p>Erro ao deletar fabricante</p>"
 
 @get('/listaFabricante')
 def listaFabricante(db):
-    response.content_type = 'application/json'
-    response_ = []
-    for row in db.execute('SELECT * FROM Fabricante ORDER BY nome ASC'):
-        response_.append(row)
+    try:
+        c = db.execute('SELECT codigo, descricao, localizacao, FROM fabricante')
+        result = c.fetchall()
 
-    return json.dump(response_)
+        items = []
+        for row in result:
+            items.append(Item(row['codigo'],row['descricao'],row['localizacao']))
+
+        table = ItemTable(items)
+
+        return table.__html__()
+    except Exception, e:
+        return "<p>Erro ao listar Fabricante</p>"
+
+class ItemTable(Table):
+    codigo = Col('codigo')
+    descricao = Col('descricao')
+    localizacao = Col('localizacao')
+
+class Item(object):
+    def __init__(self, codigo, descricao, localizacao):
+        self.codigo = codigo
+        self.descricao = descricao
+        self.localizacao = localizacao
 
 run(reloader=True, host='localhost', port=8002, debug=True)
